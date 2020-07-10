849eaaecf14d962f85c9d6e46505b1caa9c11df3
JarateKing <scmacintosh@upei.ca>
2020-07-10 16:46:44 -0300

    added parser to get longest streak

--- gen_readme.py
+++ gen_readme.py
@@ -1,3 +1,5 @@
+import json
+
 def queryString(start=2015, end=2020):
 	ret = "query {\n\t viewer {\n"
 	for year in range(start, end + 1):
@@ -5,5 +7,32 @@ def queryString(start=2015, end=2020):
 	
 	ret = ret + "\t}\n}"
 	return ret
+	
+def longestStreak(data):
+	startDate = "1960-01-01T00:00:00.000+00:00"
+	endDate = "1960-01-01T00:00:00.000+00:00"
+	streak = 0
+	currentStreak = False
+	for year in data["data"]["viewer"]:
+		for week in data["data"]["viewer"][year]["contributionCalendar"]["weeks"]:
+			for day in week["contributionDays"]:
+				if (day["contributionCount"] > 0):
+					if (currentStreak):
+						streak = streak + 1
+						endDate = day["date"]
+					else:
+						streak = 1
+						startDate = day["date"]
+						endDate = startDate
+						currentStreak = True
+				else:
+					currentStreak = False
+	return streak, startDate, endDate
 
-print(queryString())
+with open('test.json', 'r') as json_file:
+	data = json.load(json_file)
+	streak, startDate, endDate  = longestStreak(data)
+	print(streak)
+	print(startDate)
+	print(endDate)
+	
