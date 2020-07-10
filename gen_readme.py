0a4d28b8b7b7baf4542d8fc464d23e0b69b96156
JarateKing <scmacintosh@upei.ca>
2020-07-10 16:13:23 -0300

    formulate query

--- gen_readme.py
+++ gen_readme.py
@@ -1,1 +1,9 @@
-print('Hello World!')
+def queryString(start=2015, end=2020):
+	ret = "query {\n\t viewer {\n"
+	for year in range(start, end + 1):
+		ret = ret + "\t\tq" + str(year) + ": contributionsCollection(from: \"" + str(year) + "-01-01T00:00:00.000+00:00\") { contributionCalendar { weeks { contributionDays { date contributionCount } } } }\n"
+	
+	ret = ret + "\t}\n}"
+	return ret
+
+print(queryString())
